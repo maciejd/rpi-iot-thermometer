c539f2d4b72eb2c34348e10702b376fd7d1be571
alphaRGB <446860968@qq.com>
2021-11-25 21:57:14 +0800

    Add DHT11 srnsor

--- app.py
+++ app.py
@@ -1,36 +1,13 @@
-from flask import Flask, jsonify, abort, request, render_template
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import func
-import datetime, json
+from flask import Flask, render_template
+# import sensor
+from sensors import DHT11, DS18b20
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py')
-db = SQLAlchemy(app)    
 
-class Temps(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    temp = db.Column(db.Float)
-    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-
-@app.route('/temp/', methods = ['GET'])
-def index():
-    t = Temps.query.order_by(Temps.id.desc()).first()
-    return jsonify(temperature=t.temp, measured_at=t.timestamp)
-
-@app.route('/data/')
-def data():
-    since = datetime.datetime.now() - datetime.timedelta(hours=24)
-    t = Temps.query.filter(Temps.timestamp >= since).all()
-    return json.dumps([{"_id": i, "date": t[i].timestamp.strftime("%Y-%m-%d %H:%M"), "temp": t[i].temp}
-        for i in range(len(t))])
-
-@app.route('/')
-def graph():
-     since = datetime.datetime.now() - datetime.timedelta(hours=24)
-     last24 = Temps.query.filter(Temps.timestamp >= since)
-     now = Temps.query.order_by(Temps.id.desc()).first()
-     max = Temps.query.with_entities(func.max(Temps.temp).label("temp"), Temps.timestamp).first()
-     avg = last24.with_entities(func.avg(Temps.temp).label("temp")).first()
-     min = Temps.query.with_entities(func.min(Temps.temp).label("temp"), Temps.timestamp).first()
-     return render_template("index.html", now=now, max=max, avg=avg, min=min)
-
+@app.route("/home")
+def hello():
+    ds18b20_temperature = DS18b20.get_temperature_from_ds18b20()
+    dht11_temperature, dht11_humidity = DHT11.read_from_DHT11().values()
+    print(f'dht11_temperature:{dht11_temperature}')
+    return render_template('home.html', ds18b20_temperature=ds18b20_temperature, dht11_temperature=dht11_temperature, dht11_humidity=dht11_humidity)
+    return 'Hello'
